05223962a110b9fbc61f56eb616ca40601cc7670
Siskosee <seeskosee@seeskoseesko.com>
2023-10-16 11:52:28 -0700

    Number printer

---
+++ printnum.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * printnum - Handle different numeric format specifiers.
+ * @num: Integer value that is supposed to be printed.
+ * @base: Numeric base in which integer will be printed.
+ * Return: Number count of printed characters.
+ */
+
+int printnum(long num, int base)
+{
+	int count_char;
+	char *values = "0123456789abcdef";
+
+	if (num < 0)
+	{
+		printchar('-');
+		return (printnum(-num, base) + 1);
+	}
+	else if (num < base)
+		return (printchar(values[num]));
+
+	count_char = printnum(num / base, base);
+	return (count_char + printnum(num % base, base));
+}
