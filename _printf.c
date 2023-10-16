90851bc9c0a960fcdf1cea81d6406e6e59c7c0a1
Siskosee <seeskosee@seeskoseesko.com>
2023-10-16 11:46:10 -0700

    Producing formatted output

--- _printf.c
+++ _printf.c
@@ -8,40 +8,21 @@
 
 int _printf(const char *format, ...)
 {
-	int i, count_str, count_char = 0;
+	int i;
+	int count_char = 0;
 	va_list args;
 
 	va_start(args, format);
 
-	if (!format || (format[0] == '%' && format[1] == '\0')) /* Check NULL/Empty */
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		return (-1);
-	}
-	for (i = 0; format[i] != '\0'; i++) /* Iterate through characters */
-	{
-		if (format[i] != '%')
+		if (format[i] == '%')
 		{
-			printchar(format[i]); /* Print regular char */
-		}
-		else if (format[i + 1] == 'c')
-		{
-			printchar(va_arg(args, int)); /* Print char */
 			i++;
+			count_char += print_format(format[i], args);
 		}
-		else if (format[i + 1] == 's')
-		{
-			count_str = putstr(va_arg(args, char *)); /* Print string */
-			i++;
-			count_char += (count_str - 1); /* String minus format specifier */
-		}
-		else if (format[i + 1] == '%')
-			printchar('%'); /* Literally print '%' */
-		count_char += 1;
-	}
-	if (format[i - 1] == '%') /* See if previous char is '%' with no specifier */
-	{
-		va_end(args);
-		return (-1);
+		else
+			count_char += printchar(format[i]);
 	}
 	va_end(args);
 	return (count_char);
