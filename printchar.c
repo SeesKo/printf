8e79eba3fc1d314ba0904a5798e4cba9c6fca8e4
Siskosee <seeskosee@seeskoseesko.com>
2023-10-17 03:44:53 -0700

    Character printer

---
+++ printchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * printchar - Prints a character to the standard output.
+ * @c: The character to print.
+ * Return: 1 on success.
+ */
+int printchar(char c)
+{
+	return (write(1, &c, 1));
+}
